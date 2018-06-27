e8ac0c3fa58f849654b2333533b4c387d5c09ab1
iagolobla <iagoloureiroblanco@gmail.com>
2018-06-27 22:44:38 +0200

    Arreglo en la clase Habilidad: No se guarda la Nota Los talentos se guardan como enumeracion

--- app/src/main/java/es/rutolo/martillodeguerra/Habilidad.java
+++ app/src/main/java/es/rutolo/martillodeguerra/Habilidad.java
@@ -1,14 +1,14 @@
 package es.rutolo.martillodeguerra;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Habilidad {
     private NomHab nombre;
     private boolean avanzada;
     private Stat caracteristica;
     private String descripcion;
-    private ArrayList<Talento> talentos;
-    private String nota;
+    private ArrayList<NomTal> talentos;
 
     public Habilidad(NomHab nombre){
         this.nombre = nombre;
@@ -18,330 +18,323 @@ public class Habilidad {
                 avanzada=true;
                 caracteristica=Stat.Em;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.CONTORSIONISTA));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.IMITADOR));
-                nota="";
+                talentos.add(NomTal.CONTORSIONISTA);
+                talentos.add(NomTal.IMITADOR);
+                
                 break;
             case ADIESTRAR_ANIMALES:
                 avanzada=true;
                 caracteristica= Stat.Em;
                 descripcion="";
-                nota="";
                 break;
             case BUSCAR:
                 avanzada=false;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";
+                
                 break;
             case CARISMA:
                 avanzada=false;
                 caracteristica=Stat.Em;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.CALLEJEO));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.DON_GENTES));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.ETIQUETA));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.INTRIGANTE));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.ORADOR_EXPERTO));
-                nota="";
+                talentos.add(NomTal.CALLEJEO);
+                talentos.add(NomTal.DON_GENTES);
+                talentos.add(NomTal.ETIQUETA);
+                talentos.add(NomTal.INTRIGANTE);
+                talentos.add(NomTal.ORADOR_EXPERTO);
+                
                 break;
             case CARISMA_ANIMAL:
                 avanzada=false;
                 caracteristica=Stat.Em;
                 descripcion="";
-                nota="";
+                
                 break;
             case CANALIZACION:
                 avanzada=true;
                 caracteristica=Stat.V;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.AFINIDAD_AETHYR));
-                nota="";
+                talentos.add(NomTal.AFINIDAD_AETHYR);
+                
                 break;
             case CHARLATANERIA:
                 avanzada=true;
                 caracteristica=Stat.Em;
                 descripcion="";
-                nota="";
+                
                 break;
             case CODIGO_SECRETO:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";    // TODO Varios: batidor, templario, ladron, montaraz
+                    // TODO Varios: batidor, templario, ladron, montaraz
                 break;
             case CONDUCIR:
                 avanzada=false;
                 caracteristica=Stat.F;
                 descripcion="";
-                nota="";
                 break;
             case CONSUMIR_ALCOHOL:
                 avanzada=false;
                 caracteristica=Stat.R;
                 descripcion="";
-                nota="";
                 break;
             case COTILLEO:
                 avanzada=false;
                 caracteristica=Stat.Em;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.CALLEJEO));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.ETIQUETA));
-                nota="";
+                talentos.add(NomTal.CALLEJEO);
+                talentos.add(NomTal.ETIQUETA);
+                
                 break;
             case CRIAR_ANIMALES:
                 avanzada=false;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";
                 break;
             case DISFRAZ:
                 avanzada=false;
                 caracteristica=Stat.Em;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.IMITADOR));
-                nota="";
+                talentos.add(NomTal.IMITADOR);
                 break;
             case ESCALAR:
                 avanzada=false;
                 caracteristica=Stat.F;
                 descripcion="";
-                nota="";
                 break;
             case ESCONDERSE:
                 avanzada=false;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.ERRANTE));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.GATO_CALLEJERO));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.PERICIA_SUBTERRANEA));
-                nota="";
+                talentos.add(NomTal.ERRANTE);
+                talentos.add(NomTal.GATO_CALLEJERO);
+                talentos.add(NomTal.PERICIA_SUBTERRANEA);
                 break;
             case ESQUIVAR:
                 avanzada=true;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                nota="";
+                
                 break;
             case FORZAR_CERRADURAS:
                 avanzada=true;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.EXPERTO_TRAMPAS));
-                nota="";
+                talentos.add(NomTal.EXPERTO_TRAMPAS);
+                
                 break;
             case HABLAR_IDIOMA:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.IMITADOR));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.LINGUISTICA));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.VIAJERO_CURTIDO));
-                nota="";        //TODO Varios: breton, eltharin, estaliano, halfling, khazalid, kisleviano, nordico, reikspiel, tileano, clasico, oscura, goblin, grumbarth
+                talentos.add(NomTal.IMITADOR);
+                talentos.add(NomTal.LINGUISTICA);
+                talentos.add(NomTal.VIAJERO_CURTIDO);
+                        //TODO Varios: breton, eltharin, estaliano, halfling, khazalid, kisleviano, nordico, reikspiel, tileano, clasico, oscura, goblin, grumbarth
                 break;
             case HIPNOTISMO:
                 avanzada=true;
                 caracteristica=Stat.V;
                 descripcion="";
-                nota="";
+                
                 break;
             case INTIMIDAR:
                 avanzada=false;
                 caracteristica=Stat.F;
                 descripcion="Bajo algunas circunstancias se puede tirar por Empatia";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.AMENAZADOR));
-                nota="";
+                talentos.add(NomTal.AMENAZADOR);
+                
                 break;
             case JUGAR:
                 avanzada=false;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.GENIO_ARITMETICO));
-                nota="";
+                talentos.add(NomTal.GENIO_ARITMETICO);
+                
                 break;
             case LEER_ESCRIBIR:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.LINGUISTICA));
-                nota="";
+                talentos.add(NomTal.LINGUISTICA);
+                
                 break;
             case LEER_LABIOS:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.VISTA_EXCELENTE));
-                nota="";
+                talentos.add(NomTal.VISTA_EXCELENTE);
+                
                 break;
             case LENGUA_ARCANA:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";        // TODO Varios: Magia, demoniaca, elfico arcano
+                        // TODO Varios: Magia, demoniaca, elfico arcano
                 break;
             case LENGUA_SECRETA:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";        //TODO Varios: militar, ladrones, montaraz
+                        //TODO Varios: militar, ladrones, montaraz
                 break;
             case MANDO:
                 avanzada=false;
                 caracteristica=Stat.Em;
                 descripcion="";
-                nota="";
+                
                 break;
             case MONTAR:
                 avanzada=false;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.EQUITACION_ACROBATICA));
-                nota="";
+                talentos.add(NomTal.EQUITACION_ACROBATICA);
+                
                 break;
             case MOVIMIENTO_SILENCIOSO:
                 avanzada=false;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.GATO_CALLEJERO));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.ERRANTE));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.PERICIA_SUBTERRANEA));
-                nota="";
+                talentos.add(NomTal.GATO_CALLEJERO);
+                talentos.add(NomTal.ERRANTE);
+                talentos.add(NomTal.PERICIA_SUBTERRANEA);
+                
                 break;
             case NADAR:
                 avanzada=false;
                 caracteristica=Stat.F;
                 descripcion="";
-                nota="";
+                
                 break;
             case NAVEGAR:
                 avanzada=true;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                nota="";
+                
                 break;
             case OFICIO:
                 avanzada=true;
                 caracteristica=Stat.F;  // Varía con el oficio -_- Le pongo fuerza porque somos enanos
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.ARTESANIA_ENANA));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.DOTES_ARTISTICAS));
-                nota="";
+                talentos.add(NomTal.ARTESANIA_ENANA);
+                talentos.add(NomTal.DOTES_ARTISTICAS);
+                
                 break;
             case ORIENTACION:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.GENIO_ARITMETICO));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.ORIENTACION));
-                nota="";
+                talentos.add(NomTal.GENIO_ARITMETICO);
+                talentos.add(NomTal.ORIENTACION);
+                
                 break;
             case PERCEPCION:
                 avanzada=false;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.EXPERTO_TRAMPAS));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.GENIO_ARITMETICO));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.OIDO_AGUZADO));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.VISTA_EXCELENTE));
-                nota="";
+                talentos.add(NomTal.EXPERTO_TRAMPAS);
+                talentos.add(NomTal.GENIO_ARITMETICO);
+                talentos.add(NomTal.OIDO_AGUZADO);
+                talentos.add(NomTal.VISTA_EXCELENTE);
+                
                 break;
             case PONER_TRAMPAS:
                 avanzada=true;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                nota="";
+                
                 break;
             case PREPARAR_VENENOS:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";
+                
                 break;
             case PRESTIDIGITACION:
                 avanzada=true;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                nota="";
+                
                 break;
             case RASTREAR:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";
+                
                 break;
             case REGATEAR:
                 avanzada=false;
                 caracteristica=Stat.Em;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.NEGOCIADOR));
-                nota="";
+                talentos.add(NomTal.NEGOCIADOR);
+                
                 break;
             case REMAR:
                 avanzada=false;
                 caracteristica=Stat.F;
                 descripcion="";
-                nota="";
+                
                 break;
             case SABIDURIA_ACADEMICA:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";        // TODO Varios: Arte, astronomia, ciencias, demonologia, estrategia, filosofia, genealogia, historia, ingenieria, leyes, magia, nigromancia, runas, teología
+                        // TODO Varios: Arte, astronomia, ciencias, demonologia, estrategia, filosofia, genealogia, historia, ingenieria, leyes, magia, nigromancia, runas, teología
                 break;
             case SABIDURIA_POPULAR:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.VIAJERO_CURTIDO));
-                nota="";        // TODO Varios: bretonia,elfos, enanos, estalia, halflings, imperio, kislev, norsca, ogros, reinos fronterizos, tierras desoladas, tilea
+                talentos.add(NomTal.VIAJERO_CURTIDO);
+                        // TODO Varios: bretonia,elfos, enanos, estalia, halflings, imperio, kislev, norsca, ogros, reinos fronterizos, tierras desoladas, tilea
                 break;
             case SANAR:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.CIRUGIA));
-                nota="";
+                talentos.add(NomTal.CIRUGIA);
+                
                 break;
             case SEGUIMIENTO:
                 avanzada=true;
                 caracteristica=Stat.Ag;
                 descripcion="";
-                nota="";
+                
                 break;
             case SENTIR_MAGIA:
                 avanzada=true;
                 caracteristica=Stat.V;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.AFINIDAD_AETHYR));
-                nota="";
+                talentos.add(NomTal.AFINIDAD_AETHYR);
+                
                 break;
             case SUPERVIVENCIA:
                 avanzada=false;
                 caracteristica=Stat.I;
                 descripcion="";
-                nota="";
+                
                 break;
             case TASAR:
                 avanzada=true;
                 caracteristica=Stat.I;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.DOTES_ARTISTICAS));
-                talentos.add(new Diccionario().getTalentos().get(NomTal.NEGOCIADOR));
-                nota="";
+                talentos.add(NomTal.DOTES_ARTISTICAS);
+                talentos.add(NomTal.NEGOCIADOR);
+                
                 break;
             case TORTURA:
                 avanzada=true;
                 caracteristica=Stat.Em;
                 descripcion="";
-                talentos.add(new Diccionario().getTalentos().get(NomTal.AMENAZADOR));
-                nota="";
+                talentos.add(NomTal.AMENAZADOR);
+                
                 break;
             case VENTRILOQUIARA:
                 avanzada=true;
                 caracteristica=Stat.Em;
                 descripcion="";
-                nota="";
+                
                 break;
         }
 
