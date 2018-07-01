8b6850b639e2d627ae4755af1981004360d2a7bb
iagolobla <iagoloureiroblanco@gmail.com>
2018-07-01 20:56:24 +0200

    Relleno de talentos

--- app/src/main/java/es/rutolo/martillodeguerra/Talento.java
+++ app/src/main/java/es/rutolo/martillodeguerra/Talento.java
@@ -104,103 +104,103 @@ public class Talento {
                 descripcion="Sabes dónde pegar en combate cuerpo a cuerpo para hacer más daño. Puedes aplicar un +1 a las tiradas de daño con armas cuerpo a cuerpo.";
                 break;
             case GUERRERO_NATO:
-                descripcion="";
+                descripcion="Recibes un +5% permanente a tu Habilidad de Armas. Modifica tu perfil inicial para que refleje esta bonificación";
                 break;
             case HECHIZOS_ARMADURA:
-                descripcion="";
+                descripcion="Tus oraciones te permiten lanzar hechizos aunque lleves puesta una armadura. La penalización a tu tirada de magia por llevar armadura se reduce en -3. Por ejemplo, normalmente sufrirías una penalización de -3 por vestir una camisa de malla, pero con este talento la penalización queda anulada.";
                 break;
             case IMITADOR:
-                descripcion="";
+                descripcion="Tienes muy buen oído para las voces y los acentos y puedes reproducirlos con precisión. Recibes un +10% a las tiradas de Actuar (Actor, Bufón, Cómico, Narrador y Payaso), a las tiradas de Disfraz si éste tiene un componente verbal, y a las tiradas de Hablar Idioma si intentas hacerte pasar por nativo.";
                 break;
             case IMPERTURBABLE:
-                descripcion="";
+                descripcion="Tu inquebrantable mente es menos susceptible a sucesos enajenantes. No tienes que tirar para ver si desarrollas un transtorno hasta que no acumules 8 Puntos de Locura, y no obtienes una de forma automática hasta que no tengas 14 PL.";
                 break;
             case INQUIETANTE:
-                descripcion="";
+                descripcion="Tu abrumadora presencia perturba a tus oponentes. Tus enemigos deben conseguir una tirada de Voluntad o sufrirán una penalización de -10% a su Habilidad de Armas y con proyectiles. Pueden intentar esta tirada de resistencia cada asalto hasta que lo consigan o te pierdan de vista.";
                 break;
             case INTELECTUAL:
-                descripcion="";
+                descripcion="Recibes un +5% permanente a tu Inteligencia. Modifica tu perfil inicial para que refleje esta bonificación.";
                 break;
             case INTREPIDO:
-                descripcion="";
+                descripcion="O eres un valiente o estás loco, el caso es que no le tienes miedo a nada. Eres inmune al Miedo, y el Terror te afecta como si fuera Miedo. También eres inmune a los efectos de la habilidad Intimidar y del talento Inquietante. Consulta el Capítulo 9: El director de Juego para más información sobre el Miedo y el Terror.";
                 break;
             case INTRIGANTE:
-                descripcion="";
+                descripcion="Eres un maestro de las políticas personales. Recibes un +10% a todas las tiradas de Carisma relacionadas con Intrigas y Conspiraciones, y a las tiradas de Voluntad para resistirte al Carisma de otros.";
                 break;
             case LEVITACION:
-                descripcion="";
+                descripcion="Puedes flotar a poca distancia del suelo. Consulta el Capítulo 6: Combate, Daño y Movimiento para más información sobre el vuelo.";
                 break;
             case LINGUISTICA:
-                descripcion="";
+                descripcion="Tienes un don natural para los idiomas. Recibes un +10% a todas las tiradas de Leer/Escribir y Hablar Idioma.";
                 break;
             case LUCHA:
-                descripcion="";
+                descripcion="Eres un luchador experto. Puedes efectuar ataques sin armas con un +10% a tu Habilidad de Armas si intentas una presa. Además, recibes un +10% en todas tus tiradas de Fuerza relacionadas con presas.";
                 break;
             case MAGIA_MENOR:
-                descripcion="";
+                descripcion="Conoces un hechizo común a todas las formas de magia. La Magia menor no es un talento normal pues no es un único talento, sino varios, y cada uno debe adquirirse por separado. Cada talento de Magia Menor es un hechizo independiente, cuyo nombre se especifica entre paréntesis. Por ejemplo, Magia Menos(Disipar) es un talento distinto a Magia Menor(Caminar por el cielo). Los hechizos de Magia Menor más habituales son Alarma Mágica, Arma Bendita, Armadura de Aethyr, Caminar por el cielo, Cierre Mágico, Disipar, Mover y Silencio. Consulta el Capitulo 7: Magia para más información sobre la Magia Menor y las descripciones de los distintos hechizos. Debes tener un talento de Magia Pueril antes de poder adquirir un talento de Magia Menor.";
                 break;
             case MAGIA_OSCURA:
-                descripcion="";
+                descripcion="Sabes cómo manipular el Dhar (La Magia Negra) para que alimente tus hechizos. La Magia oscura te concede más poder, pero es mucho más peligrosa. Cuando lances un hechizo, puedes optar por usar las energías del Dhar para nutrirlo: En la tirada de lanzamiento de un hechizo de magia oscura puedes tirar 1d10 adicional y desechar el resultado más bajo. Sin embargo, todos los dados usados cuentan a la hora de determinar la Maldición de Tzcentch. Por ejemplo, un hechicero con Magia 2 que lance un hechizo de magia oscura tirará 3d10 y tomará los dos valores más altos para su lanzamiento; pero se contaran los tres dados para determinar la Maldición de Tzeentch. Si sacara 6, 6 y 6, la tirada de lanzamiento del hechizo sería 12(6+6), pero el hechizo activaría una Manifestación mayor del Caos. Debes usar Magia oscura para lanzar un hechizo del Saber oscuro. Consulta el Capitulo 7: Magia para más información sobre el lanzamiento de hechizos.";
                 break;
             case MAGIA_PUERIL:
-                descripcion="";
+                descripcion="Conoces las técnicas más básicas de la magia. La magia pueril no es un único talento sino varios. Cada uno de ellos es una competencia mágica independiente, cuya especialidad se indica entre paréntesis. Por ejemplo, la Magia Pueril (Arcana) es un talento distinto a la Magia Pueril (Divina). Los talentos más comunes de Magia Pueril son Arcana, Divina y Vulgar. Si posees un talento de Magia Pueril y tienes una característica de Magia de al menos 1, puedes intentar lanzar cualquier hechizo de la especialidad del talento. Consulta el Capítulo 7: Magia para más detalles (incluida una lista de hechizos).";
                 break;
             case MAGIA_VULGAR:
-                descripcion="";
+                descripcion="Eres un hechizero autodidacta que ha aprendido a usar magia mediante ensayo y error. Puedes lanzar hechizos de Magia Pueril (Vulgar) aunque no poseas la habilidad Lengua Arcana; eso sí, necesitas el talento de Magia Pueril (Vulgar). Debes tirar 1d10 adicional cada vez que lances un hechizo. El resultado no se añade a tu tirada de lanzamiento del hechizo, pero se tiene en cuenta para determinar si se produce la Maldición de Tzeentch. Cuando aprendas la Lengua Arcana ya no tendrás que tirar ese dado extra.";
                 break;
             case MANOS_RAPIDAS:
-                descripcion="";
+                descripcion="Eres muy diestro a la hora de tocar a tus oponentes en combate cuerpo a cuerpo mientras lanzas hechizos. Recibes un +20% a la HA para los hechizos de contacto";
                 break;
             case MEDITACION:
-                descripcion="";
+                descripcion="Puedes concentrar tu mente e ignorar el mundo que te rodea. Recibes una bonificación equivalente a tu Magia para las tiradas de lanzamiento de hechizos de magia ritual.";
                 break;
             case MUY_FUERTE:
-                descripcion="";
+                descripcion="Recibes un +5% permanente a tu Fuerza. Modifica tu perfil inicial para que refleje esta bonificación.";
                 break;
             case MUY_RESISTENTE:
-                descripcion="";
+                descripcion="Recibes un +5% permanente a tu Resistencia. Modifica tu perfil inicial para que refleje esta bonificación.";
                 break;
             case NEGOCIADOR:
-                descripcion="";
+                descripcion="Eres un hombre de negocios con un pico de oro y sabes cómo cerrar un trato. Recibes un +10% a todas tus tiradas de Tasar y Regatear.";
                 break;
             case NO_MUERTO:
-                descripcion="";
+                descripcion="Eres una criatura de la no vida, horriblemente reanimada mediante la nigromancia (ver Capitulo 7: Magia). Eres inmune al Miedo, el Terror, el aturdimiento, venenos, enfermedades y todos los hechizos, habilidades y efectos relacionados con la manipulación de las emociones y la mente.";
                 break;
             case ODIO_VISCERAL:
-                descripcion="";
+                descripcion="Tu pueblo guarda un longevo rencor a las diversas razas goblinoides. Sus rapiñas te llenan de tal ira que recibes un +5% a tu HA cada vez que atacas a orcos, goblins y hobgoblins.";
                 break;
             case OIDO_AGUZADO:
-                descripcion="";
+                descripcion="Tu sentido del oído es extraordinariamente bueno. Recibes un +20% a todas las tiradas de Percepción relacionadas con la audición.";
                 break;
             case ORADOR_EXPERTO:
-                descripcion="";
+                descripcion="Eres un comunicador tan consumado que enciendes a multitudes inmensas. Puedes afectar a cien veces la cantidad de gente normal para la habilidad de Carisma. Debes poseer el talento Don de Gentes para poder adquirir éste.";
                 break;
             case ORIENTACION:
-                descripcion="";
+                descripcion="Tienes un sentido instintivo de la dirección. Raras veces te pierdes, y siempre sabes por dónde queda el norte. Recibes un +10% a todas las tiradas de Orientación.";
                 break;
             case PARADA_VELOZ:
-                descripcion="";
+                descripcion="Al realizar un Ataque Rápido (véase Capítulo 6: Combate, Daño y Movimiento) puedes renunciar a uno de tus ataques y aprovecharlo para hacer una parada gratuita. Así si tuvieras Ataques 3, podrías lanzar dos ataques y hacer una parada con tu acción de ataque rápido. El límite de una parada por asalto se sigue aplicando.";
                 break;
             case PELEA_CALLEJERA:
-                descripcion="";
+                descripcion="Has aprendido a pelear en las calles. Puedes atacar sin armas con un +10% a tu Habilidad de Armas. Además, puedes aplicar un +1 a las tiradas de daño con ataques desarmado.";
                 break;
             case PERICIA_SUBTERRANEA:
-                descripcion="";
+                descripcion="Bajo tierra te sientes como en casa. Recibes un +10% a todas tus tiradas de Esconderse y Movimiento Silencioso en entornos subterráneos.";
                 break;
             case PIES_LIGEROS:
-                descripcion="";
+                descripcion="Recibes un +1 permanente a tu característica de Movimiento. Modifica tu perfil inicial para reflejar esta bonificación.";
                 break;
             case PISTOLERO_EXPERTO:
-                descripcion="";
+                descripcion="Puedes reducir en media acción el tiempo de recarga de todas las armas de pólvora negra. Si también posees el talento de Recarga Rápida, te beneficias de ambos (reduciendo así el tiempo de recarga de armas de pólvora negra una acción completa).";
                 break;
             case PROYECTIL_INFALIBLE:
-                descripcion="";
+                descripcion="Sabes hacia dónde apuntar los proyectiles mágicos para que hagan el máximo daño. Puedes aplicar un +1 a las tiradas de daño de los proyectiles mágicos de tus hechizos.";
                 break;
             case PUNTERIA:
-                descripcion="";
+                descripcion="Recibes un +5% permanente a tu Habilidad de Proyectiles. Modifica tu perfil inicial para que refleje esta bonificación.";
                 break;
             case RECARGA_RAPIDA:
-                descripcion="";
+                descripcion="Puedes recargar armas de proyectil con una facilidad derivada de la práctica. Se reduce el tiempo de recarga de todas las armas de proyectil en media acción. Por ejemplo, podrías recargar una ballesta con media acción, cuando lo normal es dedicarle una acción completa. Si el arma ya tiene un tiempo de recarga de media acción, esta se convierte en una acción libre. Prácticamente no tardas nada en recargar este tipo de armas, con lo cual puedes realizar ataques rápidos con ellas. Consulta el Capítulo 6: Combate, Daño y Movimiento para más información sobre los Ataques Rápidos.";
                 break;
             case RECIO:
                 descripcion="";
